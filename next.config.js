758aee795049ec574642d623fc13b81c4aa35710
Mikael Sand <msand@abo.fi>
2017-08-23 05:56:01 +0300

    Add Font and next.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  webpack: (config, { dev }) => {
+    // Perform customizations to config
+    config.module.rules.push({
+      loader: "babel-loader",
+      test: /\.js$/,
+      include: /svgs|victory/,
+      options: {
+        cacheDirectory: true,
+        presets: [["env", { modules: false }], "react", "stage-1"],
+        plugins: [
+          [
+            "transform-runtime",
+            {
+              helpers: false,
+              polyfill: false,
+              regenerator: false
+            }
+          ]
+        ]
+      }
+    });
+    // Important: return the modified config
+    return config;
+  }
+};
